d203706b97e74a730039499ebd5ee5e6f7dfcc81
oliverhaynes27 <oliverjhaynes2007@gmail.com>
2026-07-03 17:26:03 +0100

    Added comments and code layout fixing

--- JavaFileWatcher.java
+++ JavaFileWatcher.java
@@ -1,10 +1,13 @@
 import java.io.IOException;
 import java.nio.file.*;
 
+// Required imports for IO Exception and management of file handling
+
 public class JavaFileWatcher {
 
-    public static final String directoryPath = "C:\\Users\\olive\\OneDrive\\Desktop\\incoming";
+    public static final String directoryPath = "C:\\Users\\olive\\OneDrive\\Desktop\\FileTester";
 
+    // Directory path is created for files in the 'FileTester' folder on my Desktop
 
     public static void main(String[] args) {
         System.out.println("Watch Service started");
@@ -13,20 +16,29 @@ public class JavaFileWatcher {
         WatchService watchService = FileSystems.getDefault().newWatchService();
 
         Path path = Paths.get(directoryPath);
-        WatchKey watchKey = path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_DELETE);
+        path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_DELETE);
 
         while(true) {
-            for(WatchEvent<?> event : watchKey.pollEvents() ) {
-                System.out.println("Event type/kind : " + event.kind() + "file Name : " + event.context());
 
-            Path file = path.resolve((Path)event.context());
-                System.out.println("Location = " + file.toFile().getAbsolutePath() + "Name = " + file.toFile().getName() + "Timestamp = " + file.toFile().lastModified());
-            
+            WatchKey key = watchService.take();
+
+            for (WatchEvent<?> event : key.pollEvents()) {
+                Path file = path.resolve((Path)event.context());
+                System.out.println("Event : " + event.kind());
+                System.out.println("Location = " + file.toFile().getAbsolutePath());
+                System.out.println("Name = " + file.toFile().getName());
+                System.out.println("Timestamp = " + file.toFile().lastModified());
             }
+            key.reset();
         }
+
     } catch (IOException e) {
         e.printStackTrace();
+
+    } catch (InterruptedException e) {
+        e.printStackTrace();
     }
+
 
     }
 }
